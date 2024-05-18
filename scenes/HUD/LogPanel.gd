4dfb112be711017be92c629a4e1dd8a51172cbaf
Maaack <marek.belski@gmail.com>
2024-05-18 14:18:11 -0700

    Tweaks to logs.

--- scenes/HUD/LogPanel.gd
+++ scenes/HUD/LogPanel.gd
@@ -3,9 +3,7 @@ class_name LogPanel
 extends HUDPanel
 
 const LOG_LABEL_STRING = "[right]%s[/right]"
-const END_OF_RECORDING_STRING = "[END OF RECORDING]
-
-"
+const END_OF_RECORDING_STRING = "[END OF RECORDING]"
 
 @export var full_string : String :
 	set(value):
@@ -18,10 +16,11 @@ const END_OF_RECORDING_STRING = "[END OF RECORDING]
 var text_buffer : String
 
 func add_recording(new_log : String):
-	text_buffer += new_log
+	add_log(new_log)
 	text_buffer += "\n" + end_of_recording_text
 
 func add_log(new_log : String):
+	text_buffer += "\n\n"
 	text_buffer += new_log
 
 func _on_print_timer_timeout():
